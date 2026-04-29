1d625ce05bde68fbb84f61e66ca3998f8092f3fd
ultimate-soulcodex <kidbuggz@yahoo.com>
2026-04-28 21:28:56 -0400

    feat: initialize iOS platform support with Capacitor project structure and assets

---
+++ Ultimate Soul Codex/Ultimate Soul Codex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ultimate Soul Codex
+//
+//  Created by bj90-m1 on 4/28/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
