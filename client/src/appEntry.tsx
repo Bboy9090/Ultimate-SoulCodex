10ef3113cb1a388ae76d754c20d09268f9daa4ce
Bobbyblanco <182185694+Bboy9090@users.noreply.github.com>
2026-09-23 19:51:34 -0400

    perf(ios): remove duplicate query provider from boot entry

--- client/src/appEntry.tsx
+++ client/src/appEntry.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { useEffect } from "react";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient";
 import { registerSoulCodexServiceWorker } from "./lib/registerServiceWorker";
 import App from "./App";
 import "./index.css";
@@ -35,10 +33,10 @@ function BootReadyMarker() {
 }
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
+  <>
     <BootReadyMarker />
     <App />
-  </QueryClientProvider>
+  </>,
 );
 
 registerSoulCodexServiceWorker();
